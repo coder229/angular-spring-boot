d26de9c605a9d81257cdae69740050bf9162674d
Scott Allaire <coder229@gmail.com>
2015-02-22 08:30:32 -0600

    Removed security for now

--- src/main/java/com/github/coder229/SpringSecurityInitializer.java
+++
@@ -1,7 +0,0 @@
-package com.github.coder229;
-
-import org.springframework.security.web.context.AbstractSecurityWebApplicationInitializer;
-
-public class SpringSecurityInitializer extends AbstractSecurityWebApplicationInitializer {
-   //do nothing
-}
--- src/main/java/com/github/coder229/TaskRepository.java
+++
@@ -1,9 +0,0 @@
-package com.github.coder229;
-
-import org.springframework.data.mongodb.core.mapping.Document;
-import org.springframework.data.mongodb.repository.MongoRepository;
-
-@Document(collection="tasks")
-public interface TaskRepository extends MongoRepository<Task, String> {
-
-}
